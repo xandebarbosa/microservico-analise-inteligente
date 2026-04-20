f632e03a0437063bf5dccc6077755489de14a4f6
Alexandre Barbosa <xandesbarbosa@gmail.com>
2026-04-20 18:43:34 -0300

    Chore: Adição do nome da concessionária

--- src/main/java/com/coruja/dto/RadarDTO.java
+++ src/main/java/com/coruja/dto/RadarDTO.java
@@ -1,11 +1,14 @@
 package com.coruja.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RadarDTO {
     private String placa;
     private LocalDate data;
@@ -14,5 +17,8 @@ public class RadarDTO {
     private String km;
     private String sentido;
     private String praca;
+
+    // Aceita tanto no plural quanto no singular
+    @JsonAlias({"concessionarias", "concessionaria"})
     private String concessionaria;
 }
